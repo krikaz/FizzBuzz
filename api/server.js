d1a014baf6c58117a0401b7c1095a417f4527e64
krikaz <pascal.bou@protonmail.com>
2019-07-30 18:51:09 +0200

    init files

---
+++ api/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const fizzBuzzRouter = require('../fizzbuzz/fizzbuzz-router');
+
+const server = express();
+
+server.use(express.json());
+server.use('/api/', fizzBuzzRouter);
+
+server.get('/', (req, res) => {
+	res.send("It's working!");
+});
+
+module.exports = server;
